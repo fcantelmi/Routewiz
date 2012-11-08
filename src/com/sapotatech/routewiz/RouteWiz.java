3d618a321ba03061af94964f4f8bd1cf155cfcb3
Frank Cantelmi <frank.cantelmi@gmail.com>
2012-11-07 22:52:55 -0500

    a website in a WebView

---
+++ src/com/sapotatech/routewiz/RouteWiz.java
@@ -0,0 +1,24 @@
+package com.sapotatech.routewiz;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.webkit.WebView;
+
+public class RouteWiz extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_route_wiz);
+        
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.loadUrl("http://www.routewiz.net/");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_route_wiz, menu);
+        return true;
+    }
+}
